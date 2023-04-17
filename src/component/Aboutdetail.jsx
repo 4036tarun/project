edd529a3ba31ae4c8879e7dc1a956b143bbe5269
taruna <4036_20tarun@gc11.ac.in>
2023-04-17 19:30:49 +0530

    addition in about page

--- src/component/Aboutdetail.jsx
+++ src/component/Aboutdetail.jsx
@@ -3,6 +3,21 @@ import { AccordionContent } from "flowbite-react/lib/esm/components/Accordion/Ac
 import { AccordionPanel } from "flowbite-react/lib/esm/components/Accordion/AccordionPanel";
 import { AccordionTitle } from "flowbite-react/lib/esm/components/Accordion/AccordionTitle";
 
+// const links = [
+//   { name: 'Open roles', href: '#' },
+//   { name: 'Internship program', href: '#' },
+//   { name: 'Our values', href: '#' },
+//   { name: 'Meet our leadership', href: '#' },
+// ]
+
+const stats = [
+  { name: '987XXXXXXX', value: 'CONTACT NO.' },
+  { name: 'DEMOEMAIL321@EMAIL.COM', value: 'EMAIL ADDRESS' },
+  { name: '#XXX,DEMO ADDRESS', value: 'ADDRESS' },
+  { name: 'ANYTHING', value: 'TO BE UPDATED' },
+]
+
+
 export default function Myabout() {
 
   return(
@@ -14,7 +29,7 @@ export default function Myabout() {
     </AccordionTitle>
     <AccordionContent>
       <p className="mb-2 text-gray-500 dark:text-gray-400">
-        The Pet Shop is website that is designed so the animal get  
+        The Pet Shop is website to get INFORMATION regarding the buyer ,seller and vet hospitals.
       {/* </p>
       <p className="text-gray-500 dark:text-gray-400">
         Check out this guide to learn how to 
@@ -34,9 +49,9 @@ export default function Myabout() {
     </AccordionTitle>
     <AccordionContent>
       <p className="mb-2 text-gray-500 dark:text-gray-400">
-        Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.
-      </p>
-      <p className="text-gray-500 dark:text-gray-400">
+      The Pet Shop is website that is designed so that the people can complete their family with pet and the animals can get the home they need . 
+        </p>
+      {/* <p className="text-gray-500 dark:text-gray-400">
         Check out the
         <a
           href="https://flowbite.com/figma/"
@@ -45,24 +60,24 @@ export default function Myabout() {
           Figma design system
         </a>
         based on the utility classes from Tailwind CSS and components from Flowbite.
-      </p>
+      </p> */}
     </AccordionContent>
   </AccordionPanel>
   <AccordionPanel>
     <AccordionTitle>
-      How to use ?
+      How to use ? 
     </AccordionTitle>
     <AccordionContent>
       <p className="mb-2 text-gray-500 dark:text-gray-400">
-        The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.
+        People can find the data about hospitals and stuff.
       </p>
       <p className="mb-2 text-gray-500 dark:text-gray-400">
-        However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.
+        Those people who want to sell ,they can project thier infomation.
       </p>
       <p className="mb-2 text-gray-500 dark:text-gray-400">
-        Learn more about these technologies:
+      Those people who want to buy ,they can use the projected infomation to contact  the seller.
       </p>
-      <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
+      {/* <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
         <li>
           <a
             href="https://flowbite.com/pro/"
@@ -80,10 +95,66 @@ export default function Myabout() {
             Tailwind UI
           </a>
         </li>
-      </ul>
+      </ul> */}
     </AccordionContent>
   </AccordionPanel>
 </Accordion>
+<div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+      <img
+        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+        alt=""
+        className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+      />
+      <div
+        className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+        aria-hidden="true"
+      >
+        <div
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+      <div
+        className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+        aria-hidden="true"
+      >
+        <div
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Work with us</h2>
+          <p className="mt-6 text-lg leading-8 text-gray-300">
+            FOR THE BETTER WORLD.
+          </p>
+        </div>
+        <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+          {/* <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+            {links.map((link) => (
+              <a key={link.name} href={link.href}>
+                {link.name} <span aria-hidden="true">&rarr;</span>
+              </a>
+            ))}
+          </div> */}
+          <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+            {stats.map((stat) => (
+              <div key={stat.name} className="flex flex-col-reverse">
+                <dt className="text-base leading-7 text-gray-300">{stat.name}</dt>
+                <dd className="text-2xl font-bold leading-9 tracking-tight text-white">{stat.value}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
 </>
   )
 }
--- src/component/Carousel1.jsx
+++ src/component/Carousel1.jsx
@@ -4,14 +4,14 @@ export default function Mycarousel1(){
 
     return(
         <>
-        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 py-10">
+         <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
   <Carousel>
     <img
-      src="https://www.hdwallpapers.in/download/bunch_of_brown_white_black_puppy_dogs_in_blue_background_hd_dog-HD.jpg"
+      src="himage/cute_four_puppies_are_lying_down_on_a_greenfield_in_a_blur_background_hd_animals.jpg"
       alt="..."
     />
     <img
-      src="https://www.hdwallpapers.in/download/nice_picture_of_cats_hd_cat-1920x1080.jpg"
+      src="himage/four_brown_black_white_cute_cat_kittens_in_white_background_hd_cute_cat.jpg"
       alt="..."
     />
     <img
@@ -19,7 +19,7 @@ export default function Mycarousel1(){
       alt="..."
     />
     <img
-      src="https://images.pexels.com/photos/2289387/pexels-photo-2289387.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      src="himage/two_green_white_birds_are_perching_on_tree_branch_in_green_blur_bokeh_background_hd_green.jpg"
       alt="..."
     />
     <img
